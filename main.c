4d027e2cc211a82d00f6e8fa54db84cc5b65c683
abenfraj <72936702+abenfraj@users.noreply.github.com>
2022-09-16 17:00:52 +0200

    menu practically done, some of the game done too

--- main.c
+++ main.c
@@ -1,34 +1,73 @@
 #include <stdio.h>
 #define MAX_CHAR_PLAYER_NAME 16
 
-void pvp_mode_menu(){
-    char player_name1[MAX_CHAR_PLAYER_NAME] = { };
-    char player_name2[MAX_CHAR_PLAYER_NAME] = { };
-    printf("\n\nVous avez rejoins le mode Joueur contre Joueur !\nNom joueur 1 : ");
-    scanf("%s", player_name1);
-    printf("Nom joueur 2 : ");
-    scanf("%s", player_name2);
-    
-    
+int check_player_name_input(char player_name[])
+{
+    if (strlen(player_name) > MAX_CHAR_PLAYER_NAME)
+    {
+        printf("Veuillez respecter la limite de caracteres (%d caracteres max)\n\n", MAX_CHAR_PLAYER_NAME);
+        return 0;
+    }
+    return 1;
 }
 
-void pvia_mode() {
+void pvp_mode_menu()
+{
+    char player_name1[MAX_CHAR_PLAYER_NAME] = {};
+    char player_name2[MAX_CHAR_PLAYER_NAME] = {};
+    printf("\n\nVous avez rejoins le mode Joueur contre Joueur !\n");
+    while (check_player_name_input != 1)
+    {
+        printf("Nom joueur 1 : ");
+        scanf("%s", player_name1);
+    }
+    while (check_player_name_input != 1)
+    {
+        printf("Nom joueur 2 : ");
+        scanf("%s", player_name2);
+    }
+    pvp_mode(player_name1, player_name2);
+}
+
+void pvp_mode(char player_name1[], char player_name2[])
+{
+}
+
+void pvia_mode_menu_menu()
+{
     int difficulty_choice = 0;
     printf("\n1. Mode Facile\n2. Mode Difficile");
     scanf("%d", &difficulty_choice);
     switch (difficulty_choice)
     {
     case 1:
-        printf("easyia_mode()");
+        printf("easy_ia_mode()");
         break;
     case 2:
-        printf("hardia_mode()");
+        printf("hard_ia_mode()");
         break;
     default:
         printf("Cette entree n'est pas reconnue, veuillez reessayer");
-        printf("pvia_mode_menu()");
+        printf("pvia_mode_menu_menu_menu()");
         break;
     }
+}
+
+void easy_ia_mode()
+{
+    int matches[30];
+    printf("Jeu contre l'ordinateur, niveau facile avec 30 allumettes au départ.");
+    do
+    {
+        for (int i = 0; i < sizeof(matches) / 4; ++i)
+        {
+            printf("| ");
+        }
+        printf("Il reste %d allumettes\n\n", sizeof(matches) / 4);
+        int p1matches;
+        printf("J1 - Nombre d'allummettes à enlever : ");
+        scanf("%d", &matches);
+    } while (sizeof(matches) > 0);
 }
 
 void game_mode_choice()
@@ -42,7 +81,7 @@ void game_mode_choice()
         pvp_mode_menu();
         break;
     case 2:
-        pvia_mode();
+        pvia_mode_menu();
         break;
     default:
         printf("Cette entree n'est pas reconnue, veuillez reessayer");
@@ -59,13 +98,14 @@ void show_rules()
     printf("Le perdant est celui qui doit enlever la derniere allumette.\n");
     printf("Attention, il existe 3 niveaux de difficulte en version humain vs_ordi.\n");
     printf("Le ler est plutot simple a battre, donc pour les debut ants. Mais le dernier...\n");
-    printf("Contactez-moi si vous avez reussi (screenshot evidemment** ), mon e-mail est dans les credits.");
+    printf("Contactez-moi si vous avez reussi (screenshot evidemment** ), mon e-mail est dans les credits.\n\n");
 }
 
-void show_credits() {
+void show_credits()
+{
     printf("\n\n*****CREDITS*****\n\n");
     printf("COPYRIGHT © 2022\n");
-    printf("Ayoub & Siméon");
+    printf("Ayoub & Simeon\n\n");
 }
 
 int main()
@@ -77,29 +117,31 @@ int main()
     printf("\tBonjour et bienvenue dans le jeu des allumettes\n\n");
     printf("Vous allez avoir 3 menus.\nVous choisirez d'abord si vous voulez voir les regles du jeu ou non, puis le nombre d'allumettes a utiliser, et enfin le nombre de joueurs.\n");
 
-    printf("1 - Jouer\n");
-    printf("2 - Voir les regles du jeu\n");
-    printf("3 - Voir les credits\n");
-    printf("4 - Quitter\n\n");
-    printf("Votre choix : ");
-    scanf("%d", &menu_choice);
-    switch (menu_choice)
+    while (1)
     {
-    case 1:
-        game_mode_choice();
-        break;
-    case 2:
-        show_rules();
-        break;
-    case 3:
-        show_credits();
-        break;
-    case 4:
-        return 0;
-        break;
-    default:
-        printf("Cette entree n'est pas reconnue, veuillez reessayer");
-        break;
+        printf("1 - Jouer\n");
+        printf("2 - Voir les regles du jeu\n");
+        printf("3 - Voir les credits\n");
+        printf("4 - Quitter\n\n");
+        printf("Votre choix : ");
+        scanf("%d", &menu_choice);
+        switch (menu_choice)
+        {
+        case 1:
+            game_mode_choice();
+            break;
+        case 2:
+            show_rules();
+            break;
+        case 3:
+            show_credits();
+            break;
+        case 4:
+            return 0;
+        default:
+            printf("Cette entree n'est pas reconnue, veuillez reessayer");
+            break;
+        }
     }
 
     return 0;
